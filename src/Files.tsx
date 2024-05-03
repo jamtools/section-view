7676ef01814d61a5005deb9e953f63263f69a777
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-05-02 21:43:41 -0400

    Add parenthese to returned JSX

--- src/Files.tsx
+++ src/Files.tsx
@@ -8,12 +8,13 @@ export const Files: React.FC<FilesProps> = ({ files }) => {
   return (
     <div className="files">
       <span>+ Files</span>
-      {files.map((file) => <div id={file.id}>
+      {files.map((file) => (
+        <div id={file.id}>
         {file.title}
         <br></br> <br></br>
         {file.numComments + ' '}
         Comments
-      </div>)}
+      </div>))}
     </div>
   );
 };
