b74eb4f4798f67e71a56025094082bb516760183
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-01 16:00:29 -0400

    adjust indent to 4 spaces

--- src/Files.tsx
+++ src/Files.tsx
@@ -9,29 +9,29 @@ type FilesProps = {
 }
 
 export const Files: React.FC<FilesProps> = ({ files }) => {
-  const globalStore = useGlobalStore();
+    const globalStore = useGlobalStore();
 
-  return (
-    <Box className="files-container">
-      <Box className="files-grid-container">
-        <Grid container spacing={2} wrap="nowrap" className="files-grid">
-          {files.map((file) => {
-            const numComments = globalStore.getCommentsForFile(file.id).length;
+    return (
+        <Box className="files-container">
+            <Box className="files-grid-container">
+                <Grid container spacing={2} wrap="nowrap" className="files-grid">
+                    {files.map((file) => {
+                        const numComments = globalStore.getCommentsForFile(file.id).length;
 
-            return (
-              <Grid item xs={2.4} key={file.id} className="files-grid-item">
-                <Paper className="files-item">
-                  {file.title}
-                  <br></br> <br></br>
-                  {numComments}
-                  {' '}
-                  {plural('Comment', numComments)}
-                </Paper>
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Box>
-    </Box>
-  );
+                        return (
+                            <Grid item xs={2.4} key={file.id} className="files-grid-item">
+                                <Paper className="files-item">
+                                    {file.title}
+                                    <br></br> <br></br>
+                                    {numComments}
+                                    {' '}
+                                    {plural('Comment', numComments)}
+                                </Paper>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </Box>
+        </Box>
+    );
 }
