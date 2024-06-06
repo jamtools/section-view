5351b3f6d330dd2591b174d7bd0423e2e16c2c86
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-06 15:45:34 -0400

    Prepare to integrate Semantic UI library

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {LocalStorageStore} from './store/LocalStorageStore';
-import {LocalStorageClient} from './client/LocalStorageClient';
 
 window.addEventListener('load', async () => {
     const root = ReactDOM.createRoot(
