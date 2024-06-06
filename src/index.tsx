228103346ffc676290608b8d17495e55187b1099
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-06-06 16:30:47 -0400

    Create static layout

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
+
 
 window.addEventListener('load', async () => {
     const root = ReactDOM.createRoot(
