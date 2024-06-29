ca710feac38b3cc371cadeacf7ba657d61290393
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2024-06-29 14:48:07 -0400

    undo file deletions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,68 @@
+import {useState} from 'react';
 
+import './App.css';
+import './css_reset.css'
+import './index.css'
+import './section_view.css';
+import * as types from './types';
+import {GlobalStoreProvider} from './hooks/useGlobalStore';
+import SectionPage from './SectionPage';
+import {IClient} from './client/IClient';
+import {ClientProvider} from './hooks/useClient';
+import {useMount} from './hooks/useMount';
 
-import Foo from './Foo'
+type AppProps = {
+    projectId: string;
+    sectionId: string;
 
+    client: IClient;
+}
 
-const App = () => {
-   
+const App: React.FC<AppProps> = ({projectId, sectionId, client}) => {
+    const [initialProjectData, setInitialProjectData] = useState<types.FullProjectData | null>(null);
+    const [error, setError] = useState('');
+
+    useMount(async () => {
+        const projectDataOrError = await client.fetchFullDataForProject(projectId);
+
+        if (projectDataOrError instanceof Error) {
+            alert(projectDataOrError.message);
+            setError(projectDataOrError.message);
+            return;
+        }
+
+        setInitialProjectData(projectDataOrError);
+    });
+
+    if (error) {
+        return (
+            <p>
+                {error}
+            </p>
+        );
+    }
+
+    if (!initialProjectData) {
+        return (
+            <p>
+                Loading
+            </p>
+        );
+    }
+
+    const pageContent = (
+        <SectionPage
+            projectId={projectId}
+            sectionId={sectionId}
+        />
+    );
 
     return (
-        <Foo />
+        <ClientProvider client={client}>
+            <GlobalStoreProvider initialProjectData={initialProjectData}>
+                {pageContent}
+            </GlobalStoreProvider>
+        </ClientProvider>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'semantic-ui-css/semantic.min.css';
-
+import {LocalStorageStore} from './store/LocalStorageStore';
+import {LocalStorageClient} from './client/LocalStorageClient';
 
 window.addEventListener('load', async () => {
     const root = ReactDOM.createRoot(
         document.getElementById('root') as HTMLElement
     );
 
+    const localStore = new LocalStorageStore(localStorage);
+    const localClient = new LocalStorageClient(localStore);
+
+    const projectId = 'project-1';
+    const sectionId = 'section-1';
+
     root.render(
         <React.StrictMode>
-            <App />
+            <App
+                projectId={projectId}
+                sectionId={sectionId}
+                client={localClient}
+            />
         </React.StrictMode>
     );
 });
